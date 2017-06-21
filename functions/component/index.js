7050340f419ed21d71c480f4d25c3260a052edeb
Matthias Luebken <matthias.luebken@gmail.com>
2017-06-21 09:20:49 +0200

    eslint

--- functions/component/index.js
+++ functions/component/index.js
@@ -1,60 +1,58 @@
 console.log('starting function')
 
-const AWS = require('aws-sdk');
+const AWS = require('aws-sdk')
 
 exports.handle = function (e, ctx, cb) {
-
   var ecosystem = e.pathParameters.ecosystem.toLowerCase()
-  var package = e.pathParameters.package.toLowerCase()
+  var pkg = e.pathParameters.package.toLowerCase()
 
-  const s3 = new AWS.S3();
-  const key = "librariosio/" + ecosystem + "/" + package
+  const s3 = new AWS.S3()
+  const key = 'librariosio/' + ecosystem + '/' + pkg
   var params = {
     Bucket: 'stpl-data',
-    Key: key,
-  };
+    Key: key
+  }
 
   s3.getObject(params, function (err, data) {
     if (err) {
       console.log('Error from s3.getObject: ' + err + ' data:' + data)
 
       // publish a component query request
-      var sns = new AWS.SNS();
-      var message = {ecosystem: ecosystem, package: package} 
+      var sns = new AWS.SNS()
+      var message = { ecosystem: ecosystem, package: pkg }
       sns.publish({
         Message: JSON.stringify(message),
         TopicArn: 'arn:aws:sns:us-east-1:339468856116:stpl-component-request'
       }, function (err, data) {
         if (err) {
-          console.log(err.stack);
-          return;
+          console.log(err.stack)
+          return
         }
-        console.log('push sent');
-        console.log(data);
-      });
+        console.log('push sent')
+        console.log(data)
+      })
 
       const response = {
         statusCode: 404,
         body: JSON.stringify({
           message: "Sorry we couldn't find the package you are looking for. Please come back in a couple of seconds.",
-          err: err,
+          err: err
         })
-      };
-      cb(null, response);
+      }
+      cb(null, response)
       return
     }
 
-    console.log("--- why ???")
+    console.log('--- why ???')
 
-    let objectData = JSON.parse(data.Body.toString('utf-8'));
-    console.log('Data from s3.getObject: ' + objectData);
+    let objectData = JSON.parse(data.Body.toString('utf-8'))
+    console.log('Data from s3.getObject: ' + objectData)
     const response = {
       statusCode: 200,
       body: JSON.stringify({
-        librariosio: objectData,
+        librariosio: objectData
       })
-    };
-    cb(null, response);
-  });
-
+    }
+    cb(null, response)
+  })
 }
--- functions/root/index.js
+++ functions/root/index.js
@@ -1,12 +1,12 @@
 console.log('starting function')
-exports.handle = function(e, ctx, cb) {
+exports.handle = function (e, ctx, cb) {
   console.log('processing event: %j', e)
-  cb(null, { 
+  cb(null, {
     message: 'Welcome to Stpl API',
     version: 'pre-alpha',
     avialable_resources: [
       {
-        path : "/component/{ecosystem}/{package}"
+        path: '/component/{ecosystem}/{package}'
       }
     ]
   })
