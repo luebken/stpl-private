8348c93fee5c727c3834aa5a568ec6d163084fa5
Matthias Luebken <matthias.luebken@gmail.com>
2017-06-23 16:40:49 +0200

    cors & frontend

--- functions/component/index.js
+++ functions/component/index.js
@@ -46,6 +46,12 @@ exports.handle = function (e, ctx, mainCb) {
       }
       const response = {
         statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+          'Access-Control-Allow-Credentials': true,
+          'Content-Type': 'application/json'
+        },
         body: JSON.stringify({
           librariosio: librariosioDataBody,
           versioneye: versioneyeDataBody
