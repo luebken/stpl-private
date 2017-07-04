43f4bea33ccad4eda5b128d6d678f79cef460398
Matthias Luebken <matthias.luebken@gmail.com>
2017-07-04 11:38:18 +0200

    sending 404 if component is not found

--- functions/gql/index.js
+++ functions/gql/index.js
@@ -9,26 +9,39 @@ function runQuery (query, claims, variables) {
 
 module.exports.handle = (event, context, cb) => {
   console.log('Received event', JSON.stringify(event))
-
   const userInfo = event.requestContext.authorizer.claims
   console.log(`Event from user ${userInfo.name} with ID ${userInfo.sub}`)
 
   const request = JSON.parse(event.body)
   console.log('Query: ' + request.query)
-  console.log('variables: ' + JSON.stringify(request.variables))
+  console.log('Variables: ' + JSON.stringify(request.variables))
 
   return runQuery(request.query, userInfo, request.variables)
     .then(response => {
-      console.log(response)
-      const respified = {
-        statusCode: 200,
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        body: JSON.stringify(response)
+      if (response.errors && response.errors.length > 0) {
+        const restified = {
+          statusCode: 404,
+          headers: { 'Access-Control-Allow-Origin': '*' },
+          body: JSON.stringify(response.errors)
+        }
+        console.log('Built response ', restified)
+        return restified
+      } else {
+        const restified = {
+          statusCode: 200,
+          headers: { 'Access-Control-Allow-Origin': '*' },
+          body: JSON.stringify(response.data)
+        }
+        console.log('Built response ', restified)
+        return restified
       }
-      console.log('Built response')
-      console.log(respified)
-      return respified
     })
-    .then(response => cb(null, response))
-    .catch(err => cb(err))
+    .then(response => {
+      console.log('calling callback with response: ', response)
+      cb(null, response)
+    })
+    .catch(err => {
+      console.log('err after runQuery ' + err)
+      cb(err)
+    })
 }
