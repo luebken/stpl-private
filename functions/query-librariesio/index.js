ccf0ba6dfad393ad5c11a6abd234feef6d4b045d
Matthias Luebken <matthias.luebken@gmail.com>
2017-07-04 21:37:47 +0200

    introduced s3 lib

--- functions/query-librariesio/index.js
+++ functions/query-librariesio/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const AWS = require('aws-sdk')
 const utils = require('lib/utils-http')
+const myS3 = require('lib/utils-s3')
 
 const LIBRARIES_IO_API_KEY = process.env.LIBRARIES_IO_API_KEY
 
@@ -21,54 +21,13 @@ exports.handle = (event, context, mainCallback) => {
       console.log('error:', err)
       mainCallback(err)
     }
-    saveToS3(json, function (err, message) {
-      if (err != null) {
-        console.log('error:', err)
-        mainCallback(err)
-      }
-      console.log('sucess. stored json to s3')
+    const key = 'librariosio/' + json.platform.toLowerCase() + '/' + json.name.toLowerCase()
+    myS3.SaveJsonToS3(key, json).then((msg) => {
+      console.log('success: SaveJsonToS3', msg)
       mainCallback()
+    }).catch((err) => {
+      console.log('error:', err)
+      mainCallback(err)
     })
   })
 }
-
-// Get JSON for url. Calls callback with: err, message
-var saveToS3 = (librariosioResult, callback) => {
-  const s3 = new AWS.S3()
-
-  const key = 'librariosio/' + librariosioResult.platform.toLowerCase() + '/' + librariosioResult.name.toLowerCase()
-  var buf = new Buffer.from(JSON.stringify(librariosioResult))
-
-  var params = {
-    Bucket: 'stpl-data',
-    Key: key,
-    ContentType: 'application/json',
-    ACL: 'public-read',
-    Body: buf
-  }
-
-  s3.putObject(params, function (err, data) {
-    if (err) {
-      console.log('Error from s3.putObject: ', err, data)
-      callback(err, 'Error')
-    } else {
-      console.log('Data from s3.putObject: ', data)
-      callback(null, 'Data stored in S3')
-    }
-  })
-}
-
-/*
-  const params = {
-      TableName: process.env.DYNAMODB_TABLE_LIBRARIESIO,
-      Item: librariosio_result,
-    };
-
-    dynamoDb.put(params, (error) => {
-      // handle potential errors
-      if (error) {
-        console.error(error);
-        callback(new Error('Couldn\'t create the item.'));
-        return;
-      }
-*/
--- functions/query-versioneye/index.js
+++ functions/query-versioneye/index.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const utils = require('lib/utils-http')
-const AWS = require('aws-sdk')
+const myHttp = require('lib/utils-http')
+const myS3 = require('lib/utils-s3')
 
 const VERSIONEYE_API_KEY = process.env.VERSIONEYE_API_KEY
 
@@ -24,45 +24,18 @@ exports.handle = (event, context, mainCallback) => {
 
   const url = 'https://www.versioneye.com/api/v2/products/' + product + '/' + encodeURIComponent(pkg) + '?api_key=' + VERSIONEYE_API_KEY
 
-  utils.httpsGetJSON(url, function (err, json) {
+  myHttp.httpsGetJSON(url, function (err, json) {
     if (err != null) {
       console.log('error:', err)
       mainCallback(err)
     }
-    saveToS3(ecosystem, pkg, json).then((msg) => {
-      console.log(msg)
+    const key = 'versioneye/' + ecosystem + '/' + pkg
+    myS3.SaveJsonToS3(key, json).then((msg) => {
+      console.log('success: SaveJsonToS3', msg)
       mainCallback()
     }).catch((err) => {
       console.log('error:', err)
       mainCallback(err)
-    })
-  })
-
-  // Get JSON for url. returns promise
-  var saveToS3 = (ecosystem, pkg, versionEyeResult) => new Promise((resolve, reject) => {
-    const s3 = new AWS.S3()
-
-    const key = 'versioneye/' + ecosystem + '/' + pkg
-    var buf = new Buffer.from(JSON.stringify(versionEyeResult))
-
-    var params = {
-      Bucket: 'stpl-data',
-      Key: key,
-      ContentType: 'application/json',
-      ACL: 'public-read',
-      Body: buf
-    }
-
-    s3.putObject(params, function (err, data) {
-      if (err) {
-        console.log('Error from s3.putObject: ' + err, data)
-        // callback(err, 'Error')
-        reject(err)
-      } else {
-        console.log('Data from s3.putObject: ' + data)
-        // callback(null, 'Data stored in S3')
-        resolve('Data stored in S3')
-      }
     })
   })
 }
