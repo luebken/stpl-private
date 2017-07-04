3a7af9c7cb4da4fa4a740c1fdd5e9186953edd50
Matthias Luebken <matthias.luebken@gmail.com>
2017-07-04 11:10:22 +0200

    more cors

--- functions/root/index.js
+++ functions/root/index.js
@@ -1,16 +1,22 @@
 console.log('starting function')
-exports.handle = function (e, ctx, cb) {
-  console.log('processing event:', e)
+exports.handle = function (event, context, callback) {
+  console.log('processing event:', event)
+  console.log('with context:', context)
 
   var response = {
-    message: `Welcome ${e.requestContext.authorizer.claims.name} to Stpl API`,
+    message1: `Welcome ${event.requestContext.authorizer.claims.name} to Stpl API`,
     version: 'pre-alpha',
-    body: e
+    event: event
   }
 
-  cb(null, {
+  callback(null, {
     statusCode: 200,
-    headers: { 'Access-Control-Allow-Origin': '*' },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
+      'Access-Control-Allow-Credentials': true,
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify(response)
   })
 }
