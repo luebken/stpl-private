639f8c5a45ba41a6faa572bdeab74018c38739a9
Matthias Luebken <matthias.luebken@gmail.com>
2017-09-05 16:55:49 +0200

    beautify

--- stplui/src/Home.js
+++ stplui/src/Home.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import { Card, Icon, Divider } from 'semantic-ui-react'
+import { Card, Icon } from 'semantic-ui-react'
 
 
 class Home extends Component {
-  render () {
-      return (
-        <div>
-          <h2 style={{ 'textAlign': 'center', 'padding': '50px' }}> The shazam for apps </h2>
+  render() {
+    return (
+      <div style={{ background: 'url(bg.jpg)' }}>
+        <h1 style={{ 'textAlign': 'center', 'padding': '50px', 'color': 'white' }}> <i className="pied piper hat icon"></i> SHAZAM FOR APPS</h1>
 
-          <p style={{ 'textAlign': 'center', 'padding': '50px', 'fontSize': 'large' }}>
-          It’s time to get sanity back into software development. <br/>
-          Choose the right packages and build up sustainable, secure and compliant applications.
-          </p>
-
-        <Divider style={{ 'margin': '50px' }}/>
+        <div style={{ 'margin': '50px', 'padding': '150px' }} />
 
         <Card.Group>
           {/* hack */}
           <Card href='#' style={{ 'marginLeft': '35px' }}>
             <Card.Content>
-              <Card.Header> Vulnerabilty <Icon name='tag' /> </Card.Header>
+              <Card.Header> Security <Icon name='lock' /> </Card.Header>
               <Card.Meta> Subheading </Card.Meta>
               <Card.Description> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </Card.Description>
             </Card.Content>
@@ -27,32 +22,24 @@ class Home extends Component {
 
           <Card href='#'>
             <Card.Content>
-              <Card.Header> Vulnerabilty <Icon name='tag' /> </Card.Header>
-              <Card.Meta> Subheading </Card.Meta>
-              <Card.Description> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </Card.Description>
-            </Card.Content>
-          </Card>
-          
-          <Card href='#'>
-            <Card.Content>
-              <Card.Header> Vulnerabilty <Icon name='tag' /> </Card.Header>
+              <Card.Header> License <Icon name='browser' /> </Card.Header>
               <Card.Meta> Subheading </Card.Meta>
               <Card.Description> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </Card.Description>
             </Card.Content>
           </Card>
 
+          <Card href='#'>
+            <Card.Content>
+              <Card.Header> Maintainance <Icon name='comments' /> </Card.Header>
+              <Card.Meta> Subheading </Card.Meta>
+              <Card.Description> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. </Card.Description>
+            </Card.Content>
+          </Card>
         </Card.Group>
 
-
-        <Divider style={{ 'margin': '50px' }}/>
-
-
-        <p>
-          TODO show some top level components from github here
-        </p>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 }
 
 export default Home;
